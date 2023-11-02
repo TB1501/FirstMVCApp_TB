f41051cf9e5517fb238448888d3476de78c6cd55
Tin Bašić <tin.basic22@gmail.com>
2023-11-02 16:53:23 +0100

    Add project files.

---
+++ FirstMVCApp_TB/Controllers/HelloWorldContoller.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using FirstMVCApp_TB.Models;
+
+namespace FirstMVCApp_TB.Controllers
+{
+    public class HelloWorldContoller : Controller
+    {
+        public static List<Dog> dogs = new List<Dog>();
+
+        public IActionResult Index()
+        {
+            return View(dogs);
+        }
+
+        public IActionResult Create()
+        {
+            Dog dog = new Dog();
+            return View(dog);
+        }
+        public IActionResult CreateDog(Dog dog)
+        {
+            dogs.Add(dog);
+            return RedirectToAction("Index");
+        }
+      
+        public string Hello()
+        {
+            return "Hello";
+        }
+    }
+}
---
+++ FirstMVCApp_TB/Models/Dog.cs
@@ -0,0 +1,8 @@
+﻿namespace FirstMVCApp_TB.Models
+{
+    public class Dog
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+    }
+}
